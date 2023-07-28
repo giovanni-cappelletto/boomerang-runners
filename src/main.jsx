ac0b8e23649e02c456d0c2d87dbf01cb3cb83788
giovanni-cappelletto <giovicapp4@gmail.com>
2023-07-28 17:27:38 +0200

    Fixed a bug

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Auth0Provider
       domain="dev-w36lj5lfag1zyy2o.us.auth0.com"
       clientId="A7qW7XPtCojDpjHODvNg0PB3TUmNp1Ob"
-      authorizationParams={{ redirect_uri: "http://localhost:5173/all-events" }}
+      authorizationParams={{
+        redirect_uri: "https://boomerang-runners.vercel.app/all-events",
+      }}
     >
       <App />
     </Auth0Provider>
