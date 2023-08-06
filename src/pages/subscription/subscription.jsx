b9f8afa1fdd9ec26bd637c4603bb028868e9ecee
giovanni-cappelletto <giovicapp4@gmail.com>
2023-08-06 22:24:59 +0200

    Build done

--- src/pages/subscription/subscription.jsx
+++ src/pages/subscription/subscription.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { fetchData } from "../../Supabase.js";
-import IsParticipating from "./isParticipating.jsx";
+import IsParticipating from "./IsParticipating.jsx";
 import SubscriptionPage from "./SubscriptionPage.jsx";
 
 const modifyParticipation = () => {
