174dc1ede977f061841a7d8482513156e0e33d9c
giovanni-cappelletto <giovicapp4@gmail.com>
2023-08-10 16:37:39 +0200

    Documentation Images Test 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ import Settings from "./pages/admin/settings.jsx";
 import Subscription from "./pages/subscription/subscription.jsx";
 import Documentation from "./pages/documentation/documentation.jsx";
 import PageNotFound from "./pages/page-not-found";
-import EventsImage1 from "./assets/documentationFiles/events/image1.png";
+import EventsImage2 from "./assets/documentationFiles/events/image2.png";
+import EventsImage3 from "./assets/documentationFiles/events/image3.png";
+import EventsImage4 from "./assets/documentationFiles/subscription/image1.png";
+import EventsImage5 from "./assets/documentationFiles/user_settings/image1.png";
+import EventsImage6 from "./assets/documentationFiles/user_settings/image2.png";
+import EventsImage7 from "./assets/documentationFiles/errors/image1.png";
+import EventsImage8 from "./assets/documentationFiles/errors/image2.png";
+import EventsImage9 from "./assets/documentationFiles/create_events/image1.png";
+import EventsImage10 from "./assets/documentationFiles/event_settings/image1.png";
+import EventsImage11 from "./assets/documentationFiles/event_settings/image2.png";
 import "./App.css";
 
 function App() {
@@ -36,7 +45,43 @@ function App() {
         />
         <Route
           path="/documentation/documentationFiles/events/image1"
-          element={<img src={EventsImage1} />}
+          element={<img src={EventsImage2} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image2"
+          element={<img src={EventsImage3} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image3"
+          element={<img src={EventsImage4} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image4"
+          element={<img src={EventsImage5} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image5"
+          element={<img src={EventsImage6} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image6"
+          element={<img src={EventsImage7} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image7"
+          element={<img src={EventsImage8} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image8"
+          element={<img src={EventsImage9} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image9"
+          element={<img src={EventsImage10} />}
+        />
+        <Route
+          path="/documentation/documentationFiles/events/image10"
+          element={<img src={EventsImage11} />}
         />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
