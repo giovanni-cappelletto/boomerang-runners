0e452823671bff187d346bb528e016297298b49e
giovanni-cappelletto <giovicapp4@gmail.com>
2023-08-11 14:03:33 +0200

    Added 404 Page

--- src/pages/page-not-found.jsx
+++ src/pages/page-not-found.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import { Player } from "@lottiefiles/react-lottie-player";
+import Button from "../components/Button.jsx";
+import animation from "../assets/subscription_animation.json";
+import PageNotFoundStyles from "../styles/page-not-found.module.css";
+
 const PageNotFound = () => {
-  return <h1>404</h1>;
+  return (
+    <main className={PageNotFoundStyles.main}>
+      <div className={PageNotFoundStyles.main__content}>
+        <h1 className={PageNotFoundStyles.main__title}>
+          Errore 4
+          <Player
+            autoplay
+            loop
+            src={animation}
+            className={PageNotFoundStyles.animation}
+          ></Player>
+          4
+        </h1>
+        <p className={PageNotFoundStyles.desc}>
+          La pagina che stai cercando non esiste più.
+        </p>
+
+        <Link to="/">
+          <Button className="light" text="Homepage" />
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default PageNotFound;
